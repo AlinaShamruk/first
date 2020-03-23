d112ce221ae73748c8d12c3e27756ee69d7d0458
alina <alina@gmail.com>
2020-03-23 10:06:19 +0300

    second

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,17 +76,17 @@ function haes(){
     };
 haes();
 
-// let a = ;
-// for (i = 0; i >= 0; i++) {
+let a = 0;
+for (i = 0; i >= 0; i++) {
 
-//     if (a <= 0) {
-//         a= a+10;
-//         console.log(-a)
-//     } else {
-//         a = a-9;
-//         console.log(a)
-//     }
-//     if (a == 5) break;
-// } console.log(a)
+    if (a <= 0) {
+        a= a+10;
+        console.log(-a)
+    } else {
+        a = a-9;
+        console.log(a)
+    }
+    if (a == 5) break;
+} console.log(a)
 
 
